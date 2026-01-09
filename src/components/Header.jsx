c7c4988ca41f510dce8f26437001b6ac6c4992e5
Dipti Roka <dipti.roka.dev@gmail.com>
2026-01-09 18:29:06 +1100

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="p-6 bg-blue-600 text-white text-center">
+      <h1 className="text-4xl font-bold">Dipti Roka Chhetri</h1>
+      <p className="text-lg mt-2">Software Engineer | React & Java | Cloud Enthusiast</p>
+    </header>
+  );
+};
