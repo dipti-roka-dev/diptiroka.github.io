8f29e2394399aa64b922d9cee8670953579acf87
Dipti Roka <dipti.roka.dev@gmail.com>
2026-01-09 23:02:25 +1100

    test:add auto-deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  base: 'dipti-roka-dev.github.io',
+  base: '/dipti-roka-dev.github.io/',
   plugins: [react()],
 });
